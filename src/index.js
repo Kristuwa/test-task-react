a50bfdec6ec69bd1181f7744f112745bcb70ad31
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-05-10 14:57:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/test-task-react">
+        <BrowserRouter basename="/tweets-card">
           <App />
         </BrowserRouter>
       </PersistGate>
